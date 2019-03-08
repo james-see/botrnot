13c52fa933415174c63cd22170b311977392aa47
James Campbell <james@jamescampbell.us>
2019-03-07 19:23:34 -0800

    bug fixes and edge case handling for when a user doesnt have tweets

--- botrnot/botrnot.py
+++ botrnot/botrnot.py
@@ -14,15 +14,15 @@ import requests
 from beautifultable import BeautifulTable
 
 # globals
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 OKBLUE = '\033[94m'
 ENDC = '\033[0m'
 logo = """
 ┌───────────────────────────┐
 │     Bot R Not   2019      |
 │    ____                   |
-|   /  __\          ____    |                
-|   \( oo          (___ \   |                  
+|   /  __\          ____    |
+|   \( oo          (___ \   |
 |   _\_o/           oo~)/   |
 |  / \|/ \         _\-_/_   |
 | / / __\ \___    / \|/  \  |
@@ -42,9 +42,12 @@ PARSER = argparse.ArgumentParser(description='collects and processes twitter dat
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 PARSER.add_argument('-u', '--user', dest='username',
                     help='username to evaluate', default='jamescampbell', required=True)
-PARSER.add_argument('-n', '--no-logo', dest='nologo', action='store_true', default=False, help='dont display logo (default False)')
-PARSER.add_argument('-v', '--verbose', dest='verbose', action='store_true', default=False, help='print more things out about search')
-PARSER.add_argument('-j', '--json', dest='jsonout', action='store_true', default=False, help='save tweets out to json file')
+PARSER.add_argument('-n', '--no-logo', dest='nologo', action='store_true',
+                    default=False, help='dont display logo (default False)')
+PARSER.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                    default=False, help='print more things out about search')
+PARSER.add_argument('-j', '--json', dest='jsonout', action='store_true',
+                    default=False, help='save tweets out to json file')
 args = PARSER.parse_args()
 
 
@@ -53,9 +56,27 @@ args = PARSER.parse_args()
 
 def get_user_data(username):
     """Get user info for profile."""
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Referer': f'https://twitter.com/{username}',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.3 Safari/603.3.8',
+        'X-Twitter-Active-User': 'yes',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
     userdata = dict()
-    r = requests.get(f'https://www.twitter.com/{username}')
+    r = requests.get(f'https://www.twitter.com/{username}', headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
+    inputvalue = soup.find('input', class_='json-data')
+    stringer = str(inputvalue)  # PITA twitter bs hidden value shit
+    try:
+        userdata['listed'] = stringer.split('listed_count":')[1].split(',')[0]
+    except:
+        userdata['listed'] = 'not listed'
+    try:
+        userdata['detailedcreated'] = stringer.split('created_at":"')[
+            1].split(',')[0][:-1]
+    except:
+        userdata['detailedcreated'] = "NA"
     try:
         following = soup.find('li', class_='ProfileNav-item--following')
         following = following.find('span', class_='ProfileNav-value')
@@ -80,7 +101,7 @@ def get_user_data(username):
         userdata['userbio'] = userbio.text
     except:
         userdata['userbio'] = "NA"
-    try:    
+    try:
         userlocation = soup.find('div', class_='ProfileHeaderCard-location')
         userdata['userlocation'] = userlocation.text.strip()
     except:
@@ -95,17 +116,30 @@ def get_user_data(username):
         userdata['userbirthdate'] = userbirthdate.text.strip()
     except:
         userdata['userbirthdate'] = "NA"
-    userdata['userid'] = soup.find('div', class_="tweet")['data-user-id']
+    try:
+        userdata['userid'] = soup.find('div', class_="tweet")['data-user-id']
+    except:
+        userdata['userid'] = stringer.split('profile_id":')[1].split(',')[0]
+    try:
+        userdata['total_tweets'] = stringer.split(
+            'statuses_count":')[1].split(',')[0]
+    except:
+        userdata['total_tweets'] = 'no data found'
     if args.verbose:
         t = BeautifulTable()
         # t.column_headers = ['Item', 'Value']
         t.append_row(["Full Name", OKBLUE+userdata['fullname']+ENDC])
+        t.append_row(["Total Tweets", OKBLUE+userdata['total_tweets']+ENDC])
         t.append_row(["Followers count", OKBLUE+userdata['followers']+ENDC])
         t.append_row(["Following count", OKBLUE+userdata['following']+ENDC])
+        t.append_row(["Listed count", OKBLUE+userdata['listed']+ENDC])
         t.append_row(["User Biography", OKBLUE+userdata['userbio']+ENDC])
         t.append_row(["User Location", OKBLUE+userdata['userlocation']+ENDC])
         t.append_row(["User Join Date", OKBLUE+userdata['userjoindate']+ENDC])
-        t.append_row(["User Birth Date", OKBLUE+userdata['userbirthdate']+ENDC])
+        t.append_row(["User Detailed Join Date", OKBLUE +
+                      userdata['detailedcreated']+ENDC])
+        t.append_row(["User Birth Date", OKBLUE +
+                      userdata['userbirthdate']+ENDC])
         t.append_row(["User ID", OKBLUE+userdata['userid']+ENDC])
         print('\nBio table')
         print(t)
@@ -129,9 +163,13 @@ def get_tweets(username=args.username):
             actualjson = jsondata.json()
             souppage = BeautifulSoup(actualjson['items_html'], 'html.parser')
             tweets = souppage.find_all('li', class_='stream-item')
-            last_tweet = tweets[19]["data-item-id"]
+            try:
+                last_tweet = tweets[19]["data-item-id"]
+            except:
+                pages = 1
         else:
-            jsondata = requests.get(url, params={'max_position': last_tweet}, headers=headers)
+            jsondata = requests.get(
+                url, params={'max_position': last_tweet}, headers=headers)
             actualjson = jsondata.json()
             souppage = BeautifulSoup(actualjson['items_html'], 'html.parser')
             tweets = souppage.find_all('li', class_='stream-item')
@@ -149,13 +187,15 @@ def get_tweets(username=args.username):
             retweets = item.find('span', class_='ProfileTweet-action--retweet')
             retweets = retweets.find('span')['data-tweet-stat-count']
             tweetdict['rts'] = retweets
-            favorites = item.find('span', class_='ProfileTweet-action--favorite')
+            favorites = item.find(
+                'span', class_='ProfileTweet-action--favorite')
             favorites = favorites.find('span')['data-tweet-stat-count']
             tweetdict['favs'] = favorites
             replies = item.find('span', class_='ProfileTweet-action--reply')
             replies = replies.find('span')['data-tweet-stat-count']
             tweetdict['replies'] = replies
-            tweetdict['timestamp'] = item.find(class_='_timestamp')['data-time']
+            tweetdict['timestamp'] = item.find(
+                class_='_timestamp')['data-time']
             # tweetid
             tweetdict['id'] = item["data-item-id"]
             # print(tweetdict)
@@ -164,11 +204,12 @@ def get_tweets(username=args.username):
             last_tweet = tweets[19]["data-item-id"]
         pages = pages - 1
     if args.verbose:
-        t = BeautifulTable()
-        t.column_headers = [str(x) for x in alltweets[0].keys()]
-        t.append_row([x for x in alltweets[0].values()])
-        print('Most recent tweet')
-        print(t)
+        if len(alltweets) > 0:
+            t = BeautifulTable()
+            t.column_headers = [str(x) for x in alltweets[0].keys()]
+            t.append_row([x for x in alltweets[0].values()])
+            print('Most recent tweet')
+            print(t)
     return alltweets
 
 
@@ -178,7 +219,7 @@ def get_stats(tweetslist):
         if item['type'] == 'retweet':
             rttype.append(v)
             continue
-        for k,v in item.items():
+        for k, v in item.items():
             if k == 'replies':
                 replies.append(int(v))
             if k == 'rts':
@@ -186,15 +227,21 @@ def get_stats(tweetslist):
             if k == 'favs':
                 favorites.append(int(v))
     if args.verbose:
-        t = BeautifulTable()
-        t.column_headers = ["Metric", "Amount"]
-        t.append_row([f"Total tweets collected:",f"{OKBLUE}{len(tweetslist)}{ENDC}"])
-        t.append_row([f"Total that were retweets:",f"{OKBLUE}{len(rttype)} ({round(len(rttype)/len(tweetslist)*100)}%) {ENDC}"])
-        t.append_row([f"Retweets in {len(retweets)}",f"{OKBLUE}{sum(retweets)}{ENDC}"])
-        t.append_row([f"Replies in {len(replies)}",f"{OKBLUE}{sum(replies)}{ENDC}"])
-        t.append_row([f"Favorites in {len(favorites)}",f"{OKBLUE}{sum(favorites)}{ENDC}"])
-        print("\nMetrics table")
-        print(t)
+        if len(tweetslist) > 0:
+            t = BeautifulTable()
+            t.column_headers = ["Metric", "Amount"]
+            t.append_row([f"Total tweets collected:",
+                          f"{OKBLUE}{len(tweetslist)}{ENDC}"])
+            t.append_row([f"Total that were retweets:",
+                          f"{OKBLUE}{len(rttype)} ({round(len(rttype)/len(tweetslist)*100)}%) {ENDC}"])
+            t.append_row([f"Retweets in {len(retweets)}",
+                          f"{OKBLUE}{sum(retweets)}{ENDC}"])
+            t.append_row([f"Replies in {len(replies)}",
+                          f"{OKBLUE}{sum(replies)}{ENDC}"])
+            t.append_row(
+                [f"Favorites in {len(favorites)}", f"{OKBLUE}{sum(favorites)}{ENDC}"])
+            print("\nMetrics table")
+            print(t)
 
 # main section
 
@@ -208,9 +255,11 @@ def main():
     userdatadict = get_user_data(args.username)
     if args.jsonout:
         with open('{}-tweets.json'.format(args.username), 'w+') as f:
-            f.write(json.dumps(tweetslist, indent=4, sort_keys=True, default=str))
+            f.write(json.dumps(tweetslist, indent=4,
+                               sort_keys=True, default=str))
         with open('{}-profile.json'.format(args.username), 'w+') as f:
-            f.write(json.dumps(userdatadict, indent=4, sort_keys=True, default=str))
+            f.write(json.dumps(userdatadict, indent=4,
+                               sort_keys=True, default=str))
 
 
 if __name__ == "__main__":
